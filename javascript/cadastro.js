d08edfafbe10bf625ad9976cccac64e5afbdff42
unknown <fabia9065@gmail.com>
2026-05-26 21:02:53 -0300

    Adicionando novas funcionalidades

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -3,10 +3,19 @@ let descricao = document.querySelector("#descricao");
 let nome = document.querySelector("#nome");
 let link = document.querySelector("#link");
 let input_com_imagem = document.getElementById("input_com_imagem");
-let lista_de_jogos = document.querySelector(".lista_de_jogos");
+let inpuy_com_audio = document.getElementById("input_com_audio");
+let lista_de_musicas = document.querySelector(".lista_de_musicas");
 
 //Array
 let musicas = [];
+
+const Musica = (nome, descricao, imagem, audio) => ({
+  nome,
+  descricao,
+  imagem,
+  audio,
+});
+
 
 function PegarURLImagem() {
   if (input_com_imagem.files.length === 0) {
@@ -15,17 +24,49 @@ function PegarURLImagem() {
   return URL.createObjectURL(input_com_imagem.files[0]);
 }
 
-function CriarMusica() {
-  const urlImagem = PegarURLImagem();
-  const novaMusica = Musica(nome.value, descricao.value, urlImagem, link.value);
-  musicas.push(novaMusica);
-  localStorage.setItem("array_musicas", JSON.stringify(musicas));
-  alert("Música cadastrada!");
+function PegarURLAudio() {
+  if (input_com_audio.files.length === 0) {
+    return "";
+  }
+  return URL.createObjectURL(input_com_audio.files[0]);
 }
 
-const Musica = (nome, descricao, imagem, link) => ({
-  nome,
-  descricao,
-  imagem,
-  link,
-});
+function CriarMusica() {
+  const urlImagem = PegarURLImagem();
+  const urlAudio = PegarURLAudio();
+  const novaMusica = Musica(nome.value, descricao.value, urlImagem, urlAudio);
+  musicas.push(novaMusica);
+  MostrarLista();
+  
+}
+
+function MostrarLista() {
+  lista_de_musicas.innerHTML = "";
+  for (
+    let posicao_array = 0;
+    posicao_array < musicas.length;
+    posicao_array++
+  ) {
+    const div = document.createElement("div");
+    div.classList.add("card_musica");
+    div.innerHTML = `
+        <h3>${musicas[posicao_array].nome}</h3>
+        <p>${musicas[posicao_array].descricao}</p>
+        <img src = "${musicas[posicao_array].imagem}">
+        <audio src="${musicas[posicao_array].audio}" controls autoplay></audio>
+       <button onclick="RemoverElemento(${posicao_array})">
+       Remover</button>
+       </button>
+
+       `;
+    lista_de_musicas.appendChild(div);
+    alert("Música cadastrada!");
+  }
+
+}
+
+function RemoverElemento(indice) {
+  musicas.splice(indice, 1);
+  MostrarLista();
+}
+
--- javascript/favoritos.js
+++ javascript/favoritos.js
@@ -1,31 +1,31 @@
-let array_favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-let lista_de_favoritos = document.querySelector(".lista_de_favoritos");
+// let array_favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+// let lista_de_favoritos = document.querySelector(".lista_de_favoritos");
 
-function MostrarLista() {
-  lista_de_favoritos.innerHTML = "";
-  for (
-    let posicao_array = 0;
-    posicao_array < array_favoritos.length;
-    posicao_array++
-  ) {
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <h3>${array_favoritos[posicao_array].nome}</h3>
-        <p>${array_favoritos[posicao_array].descricao}</p>
-        <a href = "${array_favoritos[posicao_array].link}" target="_blank">Link do jogo</a>
-        <img width = "100px" src = "${array_favoritos[posicao_array].imagem}">
-        <button onclick="RemoverDosFavoritos(${posicao_array})"> Remover dos favoritos
-       </button>
+// function MostrarLista() {
+//   lista_de_favoritos.innerHTML = "";
+//   for (
+//     let posicao_array = 0;
+//     posicao_array < array_favoritos.length;
+//     posicao_array++
+//   ) {
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//         <h3>${array_favoritos[posicao_array].nome}</h3>
+//         <p>${array_favoritos[posicao_array].descricao}</p>
+//         <a href = "${array_favoritos[posicao_array].link}" target="_blank">Link do jogo</a>
+//         <img width = "100px" src = "${array_favoritos[posicao_array].imagem}">
+//         <button onclick="RemoverDosFavoritos(${posicao_array})"> Remover dos favoritos
+//        </button>
 
-       `;
-    lista_de_favoritos.appendChild(div);
-  }
-}
+//        `;
+//     lista_de_favoritos.appendChild(div);
+//   }
+// }
 
-function FavoritarElemento(indice) {
-  let favoritada = array_favoritos.splice(indice, 1);
-  lista_de_favoritos.push(lista_de_favoritos);
-  localStorage.setItem("favoritos", JSON.stringify(array_favoritos));
-}
+// // function RemoverDosFavoritos(indice) {
+// //   array_favoritos.splice(indice, 1);
+// //   localStorage.setItem("favoritos", JSON.stringify(array_favoritos));
+// //   MostrarLista();
+// // }
 
-MostrarLista();
+// MostrarLista();
